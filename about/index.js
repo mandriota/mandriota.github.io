e432f0bef19310f98d1197fac433648c233f5d33
Mark Mandriota <myriaxont@gmail.com>
2025-07-06 22:53:46 +0200

    feat: redesign

--- about/index.js
+++
@@ -1,8 +0,0 @@
-const checkAndRedirect = () => 
-    "portrait-primary portrait-secondary".includes(screen.orientation.type) ?
-        window.location.replace("https://mandriota.github.io/")
-        : undefined;
-
-checkAndRedirect();
-
-addEventListener("orientationchange", checkAndRedirect);
